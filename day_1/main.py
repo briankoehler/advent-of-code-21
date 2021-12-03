9fc5df89b0ddb7730198714b6c8d1a54135ded46
briankoehler <briankoehler@ufl.edu>
2021-12-03 02:45:58 -0500

    convert indentation to spaces

--- day_1/main.py
+++ day_1/main.py
@@ -2,39 +2,39 @@ import math
 
 
 def part_one():
-	prev_measurement = math.inf
-	answer = 0
+    prev_measurement = math.inf
+    answer = 0
 
-	with open('input.txt', 'r') as f:
-		for line in f:
-			measurement = int(line)
-			if measurement > prev_measurement: answer += 1
-			prev_measurement = measurement
+    with open('input.txt', 'r') as f:
+        for line in f:
+            measurement = int(line)
+            if measurement > prev_measurement: answer += 1
+            prev_measurement = measurement
 
-	print(f'Number of depth measurement increases: {answer}')
+    print(f'Number of depth measurement increases: {answer}')
 
 
 def part_two():
-	with open('input.txt', 'r') as f:
-		nums = [int(line) for line in f.readlines()]
+    with open('input.txt', 'r') as f:
+        nums = [int(line) for line in f.readlines()]
 
-	# Initialize values - current window is the first 2 elements
-	prev_measurement = math.inf
-	answer = 0
-	measurement = nums[0] + nums[1]
+    # Initialize values - current window is the first 2 elements
+    prev_measurement = math.inf
+    answer = 0
+    measurement = nums[0] + nums[1]
 
-	# Slide window of 3 elements until the end
-	for i in range(2, len(nums)):
-		measurement += nums[i]
+    # Slide window of 3 elements until the end
+    for i in range(2, len(nums)):
+        measurement += nums[i]
 
-		if measurement > prev_measurement: answer += 1
-		prev_measurement = measurement
-		
-		measurement -= nums[i - 2]
+        if measurement > prev_measurement: answer += 1
+        prev_measurement = measurement
+        
+        measurement -= nums[i - 2]
 
-	print(f'Number depth measurement increases: {answer}')
-	
+    print(f'Number depth measurement increases: {answer}')
+    
 
 if __name__ == '__main__':
-	part_one()
-	part_two()
+    part_one()
+    part_two()
--- day_3/main.py
+++ day_3/main.py
@@ -1,66 +1,66 @@
 
 def part_one():
-	with open('input.txt', 'r') as f:
-		lines = f.readlines()
+    with open('input.txt', 'r') as f:
+        lines = f.readlines()
 
-	tracker = {}
+    tracker = {}
 
-	for line in lines:
-		for index, c in enumerate(line.strip()):
-			if index not in tracker:
-				tracker[index] = {'0': 0, '1': 0}
-			tracker[index][c] += 1
+    for line in lines:
+        for index, c in enumerate(line.strip()):
+            if index not in tracker:
+                tracker[index] = {'0': 0, '1': 0}
+            tracker[index][c] += 1
 
-	gamma, epsilon = '', ''
+    gamma, epsilon = '', ''
 
-	for i in range(len(lines[0].strip())):
-		if tracker[i]['1'] > tracker[i]['0']:
-			gamma += '1'
-			epsilon += '0'
-		else:
-			gamma += '0'
-			epsilon += '1'
+    for i in range(len(lines[0].strip())):
+        if tracker[i]['1'] > tracker[i]['0']:
+            gamma += '1'
+            epsilon += '0'
+        else:
+            gamma += '0'
+            epsilon += '1'
 
-	gamma, epsilon = int(gamma, 2), int(epsilon, 2)
+    gamma, epsilon = int(gamma, 2), int(epsilon, 2)
 
-	print(f'Power consumption: {gamma * epsilon}')
+    print(f'Power consumption: {gamma * epsilon}')
 
 
 def part_two():
-	with open('input.txt', 'r') as f:
-		lines_ = f.readlines()
+    with open('input.txt', 'r') as f:
+        lines_ = f.readlines()
 
-	def get_rating(lines, criteria):
-		# Loop each index
-		for i in range(len(lines[0])):
-			# Return the final number
-			if len(lines) == 1:
-				return int(lines[0], 2)
+    def get_rating(lines, criteria):
+        # Loop each index
+        for i in range(len(lines[0])):
+            # Return the final number
+            if len(lines) == 1:
+                return int(lines[0], 2)
 
-			# Determine which number is more common at index i
-			zeros, ones = 0, 0
-			for line in lines:
-				if line[i] == '0': zeros += 1
-				if line[i] == '1': ones += 1
-			
-			# Follow provided criteria
-			if criteria == 0:
-				if zeros > ones:
-					lines = [line for line in lines if line[i] == '0']
-				else:
-					lines = [line for line in lines if line[i] == '1']
-			else:
-				if ones < zeros:
-					lines = [line for line in lines if line[i] == '1']
-				else:
-					lines = [line for line in lines if line[i] == '0']
+            # Determine which number is more common at index i
+            zeros, ones = 0, 0
+            for line in lines:
+                if line[i] == '0': zeros += 1
+                if line[i] == '1': ones += 1
+            
+            # Follow provided criteria
+            if criteria == 0:
+                if zeros > ones:
+                    lines = [line for line in lines if line[i] == '0']
+                else:
+                    lines = [line for line in lines if line[i] == '1']
+            else:
+                if ones < zeros:
+                    lines = [line for line in lines if line[i] == '1']
+                else:
+                    lines = [line for line in lines if line[i] == '0']
 
-	o_rating = get_rating(lines_, 0)
-	co2_rating = get_rating(lines_, 1)
+    o_rating = get_rating(lines_, 0)
+    co2_rating = get_rating(lines_, 1)
 
-	print(f'Life support rating: {o_rating * co2_rating}')
+    print(f'Life support rating: {o_rating * co2_rating}')
 
 
 if __name__ == '__main__':
-	part_one()
-	part_two()
+    part_one()
+    part_two()
