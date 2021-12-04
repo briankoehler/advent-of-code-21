91be44796b355d568d5706e2c55143dd45f2dd72
briankoehler <briankoehler@ufl.edu>
2021-12-04 00:43:05 -0500

    day 4 set up

---
+++ day_4/main.py
@@ -0,0 +1,18 @@
+
+def part_one():
+    ...
+    with open('input.txt') as f:
+        lines = f.readlines()
+
+    nums = lines[0]
+    
+    print(lines[:4])
+
+
+def part_two():
+    ...
+
+
+if __name__ == '__main__':
+    part_one()
+    part_two()
